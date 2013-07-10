def2ccd04a27c599f0c683c76195baa40590c162
A. A. Treitjak <a.treitjak@gmail.com>
2013-07-10 12:51:00 +0300

    Change fixtures.

---
+++ app/code/community/EcomDev/PHPUnitTest/Test/Helper/_data/dp-testCustomerSession.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'john_doe' => 1,
+    'jane_doe' => 2,
+    'unknown_customer' => '3'
+);
---
+++ app/code/community/EcomDev/PHPUnitTest/Test/Helper/_data/ex-testCustomerSession.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'john_doe' => array(
+        'name' => 'John Doe',
+        'is_logged_in' => TRUE
+    ),
+    'john_doe' => array(
+        'name' => 'John Doe',
+        'is_logged_in' => TRUE
+    ),
+    'unknown_customer' => array(
+        'is_logged_in' => TRUE
+    ),
+);
